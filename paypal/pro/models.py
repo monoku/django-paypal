de001002667616940f54fd943359f00232908813
Eric Fernandez <eric@appsumo.com>
2012-04-26 01:24:09 -0500

    Allows for passing None

--- paypal/pro/models.py
+++ paypal/pro/models.py
@@ -60,7 +60,8 @@ class PayPalNVP(Model):
     
     def init(self, request, paypal_request, paypal_response):
         """Initialize a PayPalNVP instance from a HttpRequest."""
-        self.ipaddress = request.META.get('REMOTE_ADDR', '').split(':')[0]
+        if request and request.META:
+            self.ipaddress = request.META.get('REMOTE_ADDR', '').split(':')[0]
         if hasattr(request, "user") and request.user.is_authenticated():
             self.user = request.user
 
